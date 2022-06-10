dc6866cf4bd0f96ddb3cac89d02482e69d8878a0
AnsAjxup <aajxuph@miumg.edu.gt>
2022-06-09 22:14:23 -0600

    Taller #2: precios y descuentos

---
+++ descuentos.js
@@ -0,0 +1,44 @@
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = Number(inputPrice.value);
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+    
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + " no es válido");
+    }else {
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    
+        const resultPrice = document.getElementById("resultPrice");
+        resultPrice.innerText = `El precio con descuento son: $${precioConDescuento}`;
+    }
+
+}  
